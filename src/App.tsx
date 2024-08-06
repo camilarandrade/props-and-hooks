18252c68bb72cbc2f88fc5ecc64fe482208aff3b
Camila Andrade <camila.rda@outlook.com>
2024-08-06 13:39:50 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import Task from "./components/Task/Task"
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+    <Task/>
+    </>
   )
 }
 
--- src/components/Contador/Contador.tsx
+++ src/components/Contador/Contador.tsx
@@ -1,10 +1,21 @@
 import './Contador.css'
 
 function Contador() {
+
+  //Variavel
+  let valor: number=0; 
+
+  //Função
+  function somarMaisUm(){
+    return valor + 1; 
+  }
+
+  console.log();
+
   return (
     <div className="container">
-        <p>O valor é: valor</p>
-        <button>Adicionar +1</button>
+        <p>O valor é: {valor}</p>
+        <button onClick={somarMaisUm}>Adicionar +1</button>
     </div>
   )
 }
